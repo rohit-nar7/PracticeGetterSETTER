e2bf5e674fc92b94988ebef2920c7dde8d1733ce
rohitsnarnaware7@gmail.com <rohitsnarnaware7@gmail.com>
2022-08-16 13:20:58 +0530

    created non arguement constructor and arguemented

--- src/customerData/Customer.java
+++ src/customerData/Customer.java
@@ -2,12 +2,22 @@ package customerData;
 
 public class Customer {
 
+    private String Name;
+    private   String Address;
+    private String email;
+    private int mobileNo;
+    private boolean membership;
+
+
     Customer() {
         String Name;
         String Address;
         String email;
         int mobileNo;
         boolean membership;
+    }
+    Customer(String Name,String Address,String email,int mobileNo,boolean membership) {
+        this.Name = Name;
 
     }
 }
